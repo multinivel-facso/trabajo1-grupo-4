2cf13bdc8e361cdd4663f8108c8fc77bd55c80f6
BenjaminZA <benjamin.zavala.t@gmail.com>
2025-06-25 08:28:44 -0400

    Intento de centrado
    Código de centrado del practico en el script del proc. Este no corre por conflicto entre paquetes

--- procesamiento/01proc_data.R
+++ procesamiento/01proc_data.R
@@ -16,7 +16,7 @@ rm(list = ls()) # para limpiar el entorno de trabajo
 
 # Carga datos ------------------------------------------------------------------
 
-load("input/data/WVS_Cross-National_Wave_7_Rdata_v6_0.RData")
+load("input/WVS_Cross-National_Wave_7_Rdata_v6_0.RData")
 
 
 # Limpieza de datos ------------------------------------------------------------
@@ -25,7 +25,8 @@ load("input/data/WVS_Cross-National_Wave_7_Rdata_v6_0.RData")
 ## Filtrar y seleccionar -------------------------------------------------------
 data <- `WVS_Cross-National_Wave_7_v6_0` %>% 
   select(pais=B_COUNTRY, female=Q260, nacionalismo=Q254, democ, meanschooling, 
-         hdi, Q121, Q124, migrationrate, Q126, Q128, Q129, pos_pol=Q240, personal_income=Q288, unemploytotal) #con variables contextuales, sin alfabetismo 
+         hdi, Q121, Q124, migrationrate, Q126, Q128, Q129, pos_pol=Q240, personal_income=Q288, unemploytotal,
+         happiness=Q46, Life_satisfaction=Q49 ) #con variables contextuales, sin alfabetismo 
 
 ## Remover NA's ----------------------------------------------------------------
 
@@ -75,6 +76,46 @@ data <- data %>%
   mutate(perc_mig = sum(c(Q121, Q124, Q126, Q128, Q129))) %>% 
   ungroup() #Escala sumativa percepción de migración
 
+data <- data %>%
+  group_by(pais) %>%
+  mutate(happines_promedio_pais = mean(happiness, na.rm = TRUE)) %>%
+  ungroup()
+
+data3 <- data %>%
+  group_by(pais) %>%
+  mutate(Life_satisfaction_promedio = mean(Life_satisfaction, na.rm = TRUE)) %>%
+  ungroup()
+
+#Centrado a la Gran Media--------------------------------------------
+
+install.packages("gt")
+
+pacman::p_load(tidyverse, lme4, texreg, gt)
+
+load(file="output/data3.RData")
+
+data3 %>% 
+  nest(-schnum) %>% 
+  mutate(fit = map(data3, ~ lm(perc_mig ~ meanschooling, data=.)),
+         fit.c = map(data3, ~ lm(perc_mig ~ happiness_promedio_pais, data=.)),
+         results = map(fit, broom::tidy),
+         results.c = map(fit.c, broom::tidy)) %>% 
+  unnest(results, results.c) %>% 
+  dplyr::select(schnum, term, estimate, term1, estimate1 ) %>% 
+  pivot_wider(id_cols = "schnum", 
+              values_from=c("estimate", "estimate1"), 
+              names_from=
+                "term") %>% 
+  rename(Intercept = "estimate_(Intercept)", Slope=estimate_homework,
+         Intercept.gmc = "estimate1_(Intercept)", Slope.gmc=estimate1_homework)%>%
+  unnest() %>% 
+  mutate_if(is.numeric, round, 2) %>% 
+  gt::gt()
+
+
 # Guardar datos ----sum()# Guardar datos ----------------------------------------------------------------
-save(data, file="Github/trabajo1-grupo-4/output/data.rdata")
+save(data3, file="output/data3.rdata")
+
+getwd()
+ls()  # para ver los objetos disponibles
 
